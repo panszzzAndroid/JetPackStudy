32ae6ae29d8f82ee42682f6a80d6b2035a062e2a
panszzzAndroid <panszzz@163.com>
2021-10-19 01:41:22 +0800

    First Commit.2021/10/19 1:00

--- app/src/main/java/com/learn/jetpackStudy/MainActivity.java
+++ app/src/main/java/com/learn/jetpackStudy/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Git!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Git2021!", Toast.LENGTH_SHORT).show();
     }
 }
